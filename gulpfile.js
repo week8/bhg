fcdf83d4a2951c384b80b665b9ff075f047581a0
linxiaoping <864337477@qq.com>
2020-05-14 10:27:40 +0800

    添加二级子页面

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var fileinclude = require('gulp-file-include');
  
 gulp.task('fileinclude', function () {
     // 适配page中所有文件夹下的所有html，排除page下的include文件夹中html
-    gulp.src(['page/**/*.html', '!page/include/**.html'])
+    gulp.src(['src/page/**/*.html', '!src/page/include/**.html'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('dist'));
 });
- 
+
 gulp.task('watch', function () {
-    gulp.watch('page/**/*.html', ['fileinclude']);
+    gulp.watch('src/page/**/*.html', ['fileinclude']);
 });
