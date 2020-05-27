7c82a57e5aa7d92866601abb283f7cf29330b50b
linxiaoping <864337477@qq.com>
2020-05-27 17:49:02 +0800

    添加less插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var fileinclude = require('gulp-file-include'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    cssmin = require('gulp-minify-css');
  
 gulp.task('fileinclude', function () {
     // 适配page中所有文件夹下的所有html，排除page下的include文件夹中html
@@ -12,15 +13,19 @@ gulp.task('fileinclude', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/page/**/*.html', ['fileinclude']);
-});
-
-//定义一个testLess任务（自定义任务名称）
-gulp.task('testLess', function () {
-    gulp.src('src/assets/css/**/*.less') //该任务针对的文件
+//定义一个less任务（自定义任务名称）
+gulp.task('less', function () {
+    gulp.src(['src/assets/css/*.less', '!src/assets/css/**/{reset,test}.less']) //该任务针对的文件
         .pipe(less()) //该任务调用的模块
-        .pipe(gulp.dest('dist/assets/css/**')); //将会在src/css下生成index.css
+        .pipe(cssmin()) //该任务调用的模块
+        .pipe(gulp.dest('dist/assets/css')); //将会在src/css下生成index.css
+    //gulp.watch('src/assets/css/*.less', ['less']); //定义默认任务
 });
 
-gulp.task('default',['testLess']); //定义默认任务
+// gulp.task('watch', function () {
+//     gulp.watch('src/page/**/*.html', ['fileinclude']);
+//     gulp.watch('src/assets/css/*.less', ['less']);
+// });
+
+//gulp.task('default',['less']); //定义默认任务
+
